59dd0007c389af1369697bbb736afe1cece2529b
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-01-21 12:53:16 +0300

    Seed Data In Tables

--- HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
