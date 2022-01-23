eebfbdda413bb7e486896dd4d2c44719534b7ed9
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-01-23 22:53:49 +0300

    Write Unit Tests for Application

---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Commands
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepository;
+        private readonly CreateLeaveTypeDto _createLeaveTypeDto;
+        private readonly CreateLeaveTypeCommandHandler _handler;
+
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+            _handler = new CreateLeaveTypeCommandHandler(_mockRepository.Object, _mapper);
+            _createLeaveTypeDto = new CreateLeaveTypeDto
+            {
+                DefaultDays = 15,
+                Name = "Test DTO"
+            };
+        }
+
+        [Fact]
+        public async Task Valid_LeaveType_Added()
+        {
+            var result = await _handler.Handle(new CreateLeaveTypeCommand { LeaveTypeDto = _createLeaveTypeDto }, CancellationToken.None);
+
+            var leaveTypes = await _mockRepository.Object.GetAll();
+
+            result.ShouldBeOfType<int>();
+            leaveTypes.Count.ShouldBe(4);
+        }
+
+        [Fact]
+        public async Task InValid_LeaveType_Added()
+        {
+            _createLeaveTypeDto.DefaultDays = -1;
+            var exception = await Should.ThrowAsync<ValidationException>(async () =>
+                await _handler.Handle(new CreateLeaveTypeCommand { LeaveTypeDto = _createLeaveTypeDto },
+                    CancellationToken.None));
+
+            var leaveTypes = await _mockRepository.Object.GetAll();
+
+            leaveTypes.Count.ShouldBe(3);
+
+            exception.ShouldNotBeNull();
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public static class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    DefaultDays = 10,
+                    Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 15,
+                    Name = "Test Sick"
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    DefaultDays = 12,
+                    Name = "Test Maternity"
+                }
+              };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(m => m.GetAll()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(m => m.Add(It.IsAny<LeaveType>())).ReturnsAsync((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return leaveType;
+            });
+
+            return mockRepo;
+        }
+    }
+}
